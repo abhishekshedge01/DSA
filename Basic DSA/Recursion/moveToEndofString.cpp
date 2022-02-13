89e877991732eb9b830440fbf043ed52d0fb73d5
Abhishek Shedge <abhishedge01@gmail.com>
2022-02-13 23:29:19 +0530

    2 approaches to shift char x at the end of the string

--- Basic DSA/Recursion/moveToEndofString.cpp
+++ Basic DSA/Recursion/moveToEndofString.cpp
@@ -16,11 +16,34 @@ string movetoLast(string str,char x)
     return first+ans;
 }
 
+string putAtEnd(string str,char x)
+{
+	if (str.length() == 0)
+	{
+		return "";
+	}
+	if (str[0] == str[1])
+	{
+		return putAtEnd(str.substr(1),x) + str[0];
+	}
+	else
+	{
+		return str[0] + putAtEnd(str.substr(1),x);
+	}
+}
+
 int main()
 {
    string str;
    cin>>str;
    char a;
    cin>>a;
-   cout<<movetoLast(str,a);
+   cout<<movetoLast(str,a)<<endl;
+
+
+   	string strx;
+	cin >> strx;
+	char x;
+	cin >> x;
+	cout << movetoLast(strx, x);
 }
