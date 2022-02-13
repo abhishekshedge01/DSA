46925b61a20a69fe2f96a9a5b9df51a87559a09f
Abhishek Shedge <abhishedge01@gmail.com>
2022-02-13 19:58:48 +0530

    2 approaches for removing a char x

--- Basic DSA/Recursion/removeChar.cpp
+++ Basic DSA/Recursion/removeChar.cpp
@@ -1,29 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void removeX(char* str,char x)
+
+string removeString(string str, char x)
 {
-    if(str[0]=='\0')
+    if (str.length() == 0)
+    {
+        return "";
+    }
+    if (str[0] == x)
+    {
+        return removeString(str.substr(1), x);
+    }
+    else
+    {
+        return str[0] + removeString(str.substr(1), x);
+    }
+}
+
+void removeX(char* str, char x)
+{
+    if (str[0] == '\0')
     {
         return;
     }
-    removeX(str+1,x);
-    if(str[0]==x){
-        int i=1;
-        for(;i<str[i]!='\0';++i)
+    removeX(str + 1, x);
+    if (str[0] == x) {
+        int i = 1;
+        for (; i < str[i] != '\0'; ++i)
         {
-            str[i-1]=str[i];
+            str[i - 1] = str[i];
         }
-        str[i-1]='\0';
+        str[i - 1] = '\0';
     }
 }
 
 int main()
 {
     char str[100];
-    cin>>str;
+    cin >> str;
     char x;
-    cin>>x;
-    removeX(str,x);
-    cout<<str<<endl;
+    cin >> x;
+    removeX(str, x);
+    cout << str << endl;
+    string newstr;
+    cin >> newstr;
+    cout<<removeString(newstr, x);
 }
