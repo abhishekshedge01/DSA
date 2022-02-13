b71c0f4058eaea3b961ce157591090a956dbd01e
Abhishek Shedge <abhishedge01@gmail.com>
2022-02-13 12:13:00 +0530

    reverse array 3 methods

--- Basic DSA/Recursion/reverseArray.cpp
+++ Basic DSA/Recursion/reverseArray.cpp
@@ -1,54 +1,61 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// void insert(vector <int> &vec,int temp)
-// {
-//     if(vec.size()==0)
-//     {
-//         vec.push_back(temp);
-//     }
-//     int val=vec[vec.size()-1];
-//     insert(vec,temp);
-//     vec.push_back(val);
-// }
-
-// void reverse(vector <int> &vec)
-// {
-//     if(vec.size()==0)
-//     {
-//         return;
-//     }
-//     int temp=vec[vec.size()-1];
-//     reverse(vec);
-//     insert(vec,temp);
-// }
-
-void reverse(vector <int> &vec,int n,int i)
+void reverseVoid1(int* arr,int n)
 {
-    if(vec.size()==0)
-    {
-        return;
-    }
-    if(i>=n/2)
-    {
-        return;
-    }
-    swap(vec[i],vec[n-i-1]);
-    reverse(vec,n,++i);
+	if (n == 0)
+	{
+		return;
+	}
+	reverseVoid1(arr + 1,n-1);
+	cout << arr[0]<<" ";
+}
+
+void reverseVoid2(int *arr, int i, int n)
+{
+	if (i >= n) {
+		return;
+	}
+	int value = arr[i];
+	reverseVoid2(arr, i + 1, n);
+	arr[n - i - 1] = value;
+}
+
+void reverseVoid3(int* arr, int i, int n)
+{
+	if (i >= n/2)
+	{
+		return;
+	}
+	swap(arr[i], arr[n - i - 1]);
+	reverseVoid3(arr, ++i, n);
+}
+
+void print(int* arr,int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		cout << arr[i]<<" ";
+	}
+	cout << endl;
 }
 
 int main()
 {
-    int n;
-    cin>>n;
-    vector <int> vec(n);
-    for(int i=0;i<n;++i)
-    {
-        cin>>vec[i];
-    }
-    reverse(vec,n,0);
-    for(int i=0;i<n;++i)
-    {
-        cout<<vec[i]<<" ";
-    }
+	int n;
+	cin >> n;
+	int* arr=new int[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+
+    reverseVoid1(arr,n);
+	cout << endl;   // O(n2) approach
+
+	reverseVoid2(arr, 0, n);
+	print(arr, n);   // O(n)
+
+	reverseVoid3(arr, 0, n);
+	print(arr, n);    // O(n)
 }
