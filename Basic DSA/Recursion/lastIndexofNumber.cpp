6406e63772f19a02b4b5b761105c0b6150077d0d
Abhishek Shedge <abhishedge01@gmail.com>
2022-02-14 12:32:42 +0530

    last occurance of a number

--- Basic DSA/Recursion/lastIndexofNumber.cpp
+++ Basic DSA/Recursion/lastIndexofNumber.cpp
@@ -27,18 +27,18 @@ int lastIndexOfArray(int* arr,int n,int x)
 }
 
 
-// int alternate(int* arr,int n,int x)
-// {
-//     int ans=-1;
-//     for(int i=0;i<n;++i)
-//     {
-//         if(arr[i]==x)
-//         {
-//             ans=i;
-//         }
-//     }
-//     return ans;
-// }
+int alternate(int* arr,int n,int x)
+{
+    int ans=-1;
+    for(int i=0;i<n;++i)
+    {
+        if(arr[i]==x)
+        {
+            ans=i;
+        }
+    }
+    return ans;
+}
 
 int main()
 {
